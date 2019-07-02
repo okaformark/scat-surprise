21168496a96c5a6cc44a2dd98ff0f639481e9cde
Mark Okafor <okaformark@gmail.com>
2019-07-02 18:06:27 -0500

    auth works

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,55 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import Auth from '../components/Auth/Auth';
+
+import Home from '../components/Home/Home';
+
+import MyNavbar from '../components/MyNavbar/MyNavbar';
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className="btn btn-info">I</button>
-      </header>
-    </div>
-  );
+import fbConnection from '../helpers/data/connection';
+
+fbConnection();
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const { authed } = this.state;
+    const loadComponent = () => {
+      if (authed) {
+        return <Home />;
+      }
+      return <Auth />;
+    };
+    return (
+      <div className="App">
+        <MyNavbar authed={authed}/>
+        {loadComponent()}
+      </div>
+    );
+  }
 }
 
 export default App;
